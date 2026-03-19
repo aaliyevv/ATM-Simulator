a203770f3abfa60b1f941b94680ab2adc4a40a37
Ali <aaliyev14291@ada.edu.az>
2026-03-19 16:19:13 +0400

    Main

--- src/Main.java
+++ src/Main.java
@@ -14,4 +14,34 @@ public class Main {
         String enteredAccountNumber = scanner.nextLine();
         IBankAccount currentAccount = null;
 
-        
+        for (BankAccount ba : accounts) {
+            if (ba != null && ba.getAccountNumber().equals(enteredAccountNumber)) {
+                currentAccount = ba;
+                break;
+            }
+        }
+
+        if (currentAccount == null) {
+            System.out.println("There is no account with this number");
+            scanner.close();
+            return;
+        }
+
+        int choice;
+        do {
+            System.out.println("\n--- Main Menu ---");
+            System.out.println("1. Check balance");
+            System.out.println("2. Withdraw money");
+            System.out.println("3. Deposit money");
+            System.out.println("4. Add interest (only for Savings Account)");
+            System.out.println("5. Show overdraft limit (only for Checking Account)");
+            System.out.println("0. Exit");
+            System.out.print("Enter your choice: ");
+            choice = scanner.nextInt();
+
+            switch (choice) {
+                case 1:
+                    System.out.println(currentAccount.displayBalance());
+                    break;
+                
+}
